b45550afa57c62c1dd43321cc407a3a2d76d99e0
Natalia Leonardo <nataliajleo@gmail.com>
2023-02-15 14:39:35 -0300

    test: should return list with "-+-+-", " | | ", "-+-+-" when creating board center

--- test/playXO.test.js
+++ test/playXO.test.js
@@ -1,4 +1,4 @@
-const { generateBlock, generateSeparator } = require('../src/playXO');
+const { generateBlock, generateSeparator, generateCenter } = require('../src/playXO');
 
 describe('Validate Tic-Tac-Toe Game', () => {
 
@@ -8,5 +8,9 @@ describe('Validate Tic-Tac-Toe Game', () => {
 
     test('should return "-+-+-" when creating separator', () => {
         expect(generateSeparator()).toBe('-+-+-');
+    });
+
+    test('should return list with "-+-+-", " | | ", "-+-+-" when creating board center', () => {
+        expect(generateCenter()).toStrictEqual(['-+-+-',  ' | | ', '-+-+-']);
     });
 });
