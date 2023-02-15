75ab4b10523434dba262caf049a2275e6a7b6835
Natalia Leonardo <nataliajleo@gmail.com>
2023-02-15 14:19:04 -0300

    test: should return " | | " when creating block

---
+++ test/playXO.test.js
@@ -0,0 +1,8 @@
+const { generateBlock } = require('../src/playXO');
+
+describe('Validate Tic-Tac-Toe Game', () => {
+
+    test('should return " | | " when creating block', () => {
+        expect(generateBlock()).toBe(' | | ');
+    });
+});
