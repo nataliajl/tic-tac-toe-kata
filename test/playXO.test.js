1064fc904784d73ee34fdb109297d6bf6c28732d
Natalia Leonardo <nataliajleo@gmail.com>
2023-02-15 14:22:10 -0300

    test: should return "-+-+-" when creating separator

--- test/playXO.test.js
+++ test/playXO.test.js
@@ -1,8 +1,12 @@
-const { generateBlock } = require('../src/playXO');
+const { generateBlock, generateSeparator } = require('../src/playXO');
 
 describe('Validate Tic-Tac-Toe Game', () => {
 
     test('should return " | | " when creating block', () => {
         expect(generateBlock()).toBe(' | | ');
+    });
+
+    test('should return "-+-+-" when creating separator', () => {
+        expect(generateSeparator()).toBe('-+-+-');
     });
 });
