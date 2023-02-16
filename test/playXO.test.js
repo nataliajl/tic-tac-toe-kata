af66b3ef2b0df1bf6a563921c330e619b51c8a4a
Natalia Leonardo <nataliajleo@gmail.com>
2023-02-16 17:32:22 -0300

    test:refactor: remove break to the end of separator

--- test/playXO.test.js
+++ test/playXO.test.js
@@ -7,7 +7,7 @@ describe('Validate Tic-Tac-Toe Game', () => {
     });
 
     test('should return "-+-+-" when creating separator', () => {
-        expect(generateSeparator()).toBe('-+-+-\n');
+        expect(generateSeparator()).toBe('-+-+-');
     });
 
     test('should return " | | ", "-+-+-", " | | ", "-+-+-", " | | " with break after each line when creating board center', () => {
